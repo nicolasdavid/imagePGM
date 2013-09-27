3d32948db9bba8ab7f5a5519fcacf61a2ed7d255
Nicolas DAVID <nicolas.david@eleves.ec-nantes.fr>
2013-09-27 17:16:55 +0200

    Created the class for reading the file lena - no input of user -

---
+++ src/tp_eclipse/File.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_eclipse;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class File {
+    
+    
+    String chaine ="";
+    String fichier = "lena.pgm";
+    
+    public File(){
+    //lecture du fichier texte	
+        try{
+                InputStream ips=new FileInputStream(fichier); 
+                InputStreamReader ipsr=new InputStreamReader(ips);
+                BufferedReader br=new BufferedReader(ipsr);
+                String ligne;
+                while ((ligne=br.readLine())!=null){
+                        System.out.println(ligne);
+                        chaine+=ligne+"\n";
+                }
+                br.close(); 
+        }	
+        catch (Exception e){
+                            System.out.println(e.toString());
+                    }
+            System.out.println("chaine");
+
+        }
+}
